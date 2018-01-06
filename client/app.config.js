1ac633a4bbfef95f98cb50b5b5c91147653dd96c
suzannaaltman <suzannaaltman@gmail.com>
2018-01-06 17:01:11 -0600

    adds more fe files and styles

---
+++ client/app.config.js
@@ -0,0 +1,27 @@
+angular.module('hustleApp')
+  .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/partials/home.html',
+        controller: 'HomeController',
+        controllerAs: 'home'
+      })
+      .when('/profiles', {
+        templateUrl: 'views/partials/profiles.html',
+        controller: 'ProfileController',
+        controllerAs: 'profiles'
+      })
+      .when('/projects', {
+        templateUrl: 'views/partials/projects.html',
+        controller: 'ProjectController',
+        controllerAs: 'projects'
+      });
+
+
+  $locationProvider.html5Mode(true);
+
+  if( window.location.hash == '#!' ) {
+  window.location.href = 'http://localhost:3007';
+  }
+
+}]);//closes app.config()
