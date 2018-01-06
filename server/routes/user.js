7a6fe3330f529d7650fcabd6d1cd40c24bbf8712
Tessa Ganser <t.m.ganser@gmail.com>
2018-01-06 15:17:51 -0600

    jessica made everything work

---
+++ server/routes/user.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+var pg = require('pg');
+
+var config = {
+  database: 'htg_hustle',
+  host: 'localhost',
+  port: 5432,
+  max: 12
+};
+
+var pool = new pg.Pool( config );
+
+router.get('/', function(req, res) {
+  console.log('In the users route', req.body);
+  let users = [];
+  pool.connect(function(error, connection, done) {
+    if (error) {
+      console.log('adminRouteError', error);
+      res.sendStatus(400);
+    } //end if
+    else {
+      console.log('adminDB connection ready');
+      let resultSet = connection.query('SELECT * FROM users');
+      resultSet.on('row', function(row) {
+        adminStaff.push(row);
+      }); //row end
+      resultSet.on('end', function() {
+
+        done();
+        console.log(users);
+        res.send(users);
+      }); // resultSet end
+    } //end else
+  }); //pool.connect end
+}); //router.GET end
+
+
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,14 +1,24 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var router = express.Router();
 var pg = require('pg');
 
+var users = require('./routes/user');
+var projects = require('./routes/projects');
 
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'public/views/index.html'));
 });
+
+app.use('/users', users);
+// app.use('/projects', projects);
+// app.use('/', routes);
+// app.use(app.router);
+// routes.initialize(app);
+
 
 // server port set and listen
 var serverPort = process.env.PORT || 3007;
@@ -18,62 +28,4 @@ var server = app.listen(serverPort, function() {
   console.log('up and listening on', server.address().port);
 });
 
-var config = {
-  database: 'htg_hustle',
-  host: 'localhost',
-  port: 5432,
-  max: 12
-};
-
-var pool = new pg.Pool( config );
-
-
-app.get ('/getTasks', function(req, res){
-  console.log('get Tasks route hit' );
-
-  var allTasks = [];
-
-    pool.connect(function( err, connection, done){
-      if ( err ){
-        console.log("we're in the error state");
-        res.send("something weird is happening here.");
-
-      } else {
-        console.log('connected to db');
-        var resultSet = connection.query("SELECT * from test");
-
-        resultSet.on( 'row', function ( row ){
-          allTasks.push ( row );
-          });
-
-        resultSet.on('end', function(){
-          done();
-        res.send(allTasks);
-      });
-
-      }
-        });
-});//end app.get
-
-app.post ('/create', function(req, res){
-
-    console.log("task recieved from client: ", req.body.info);
-
-    pool.connect(function(err, connection, done){
-      if (err) {
-        // res.send("Error in /createTask route");
-        res.send(400);
-      }
-      else {
-        console.log("success! We reached the database. ");
-        //TODO need to set up the database so we can pass info to it!
-        completeStatus = 'N';
-        connection.query("INSERT into tasks(info, complete) values ($1, $2)", [req.body.info, completeStatus]);
-
-        done();
-        res.send(200);
-    }
-
-});
-
-});//end app.post
+module.exports = router;
