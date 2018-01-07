e10621b057d1cd6e8076ca68fcb02644987a6af2
Tessa Ganser <t.m.ganser@gmail.com>
2018-01-06 18:15:10 -0600

    routes work

---
+++ client/projects.js
@@ -0,0 +1,30 @@
+hustleApp.controller('ProjectController', function($scope) {
+  console.log('ProjectController loaded');
+  // var vm = this;
+  // vm.intentions = ["goal1", "goal2", "goal3", "goal4"];
+  // var newInput = vm.newInput;
+  // var hours = vm.hours;
+  // var progress = vm.progress
+
+
+  // var user = session.user;
+
+  // console.log('hours:', hours);
+  // vm.myIntentions = [];
+
+    // vm.addIntention = function(newInput, hours, userthing){
+    //   IntentionsService.addIntention(newInput, hours, userthing);
+    //   IntentionsService.updateIntentions().then(function(data){
+    //     vm.myIntentions = data;
+    //     createData(vm.myIntentions);
+    //   });
+    // };
+    //
+    // vm.removeIntention = function(thing){
+    //   IntentionsService.removeIntention(thing);
+    //   IntentionsService.updateIntentions().then(function(data){
+    //     vm.myIntentions = data;
+    //     createData(vm.myIntentions);
+    //     });
+    // };
+});
